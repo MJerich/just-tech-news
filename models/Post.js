81f75475753a5525a545c7649795f5fdba8e30a8
MJerich <MatthewJerich@gmail.com>
2022-05-04 12:01:58 -0700

    finished upvote route

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,30 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Post model
-class Post extends Model {}
+class Post extends Model {
+    static upvote(body, models) {
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            });
+        });
+    }
+}
 
 // create fields/columns for Post model
 Post.init(
@@ -13,7 +36,7 @@ Post.init(
             primaryKey: true,
             autoIncrement: true
         },
-            title: {
+        title: {
             type: DataTypes.STRING,
             allowNull: false
         },
@@ -24,7 +47,7 @@ Post.init(
                 isURL: true
             }
         },
-            user_id: {
+        user_id: {
             type: DataTypes.INTEGER,
             references: {
                 model: 'user',
@@ -39,3 +62,5 @@ Post.init(
         modelName: 'post'
     }
 );
+
+module.exports = Post;
