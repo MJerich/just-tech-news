8b143df95ea9046274d053309a20da712e42a594
MJerich <MatthewJerich@gmail.com>
2022-05-08 13:05:27 -0700

    added user authentication

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+// express session set-up
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 // server setup
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +28,9 @@ const hbs = exphbs.create({});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// session middlwware
+app.use(session(sess));
 
 // handlebars middleware
 app.engine('handlebars', hbs.engine);
