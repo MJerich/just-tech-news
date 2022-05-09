5a4b6b11bf938249a8a990fcf89c020d0315666f
MJerich <MatthewJerich@gmail.com>
2022-05-08 17:19:47 -0700

    big fix

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -28,7 +28,7 @@ router.post('/', withAuth, (req, res) => {
     }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Comment.destroy({
         where: {
             id: req.params.id
